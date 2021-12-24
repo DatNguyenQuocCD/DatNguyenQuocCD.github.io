678bf2c0581a43a2da0a5a64cbe23f8d9e84b16d
DatNguyenQuocCD <96603285+DatNguyenQuocCD@users.noreply.github.com>
2021-12-24 09:04:02 +0700

    Delete JS directory

--- JS/music.js
+++
@@ -1,9 +0,0 @@
-var sound = new Howl({
-    src: ['./y2mate (mp3cut.net).mp3'],
-    autoplay: true,
-    loop: true,
-    volume: 1,
-    onend: function() {
-        console.log('Finished!');
-    }
-});
